90f48bb8b66b0a1ea762c491b6d72b0918f367ba
SteinRZ <xddan15@gmail.com>
2023-10-03 06:31:14 -0500

    Modulo ECMA

---
+++ Parcial 2/ModuloEcma/Areas.js
@@ -0,0 +1,7 @@
+export function areaCirculo(radio){
+    return ((radio*radio)* 3.1416);
+}
+
+export function mensaje(msg){
+    return msg="Hola mundo";
+}
---
+++ Parcial 2/ModuloEcma/Calcular.js
@@ -0,0 +1,9 @@
+import * as areas from "./Areas.js";
+
+let a=areas.areaCirculo(2);
+
+console.log(a);
+
+let m=areas.mensaje();
+
+console.log(m);
