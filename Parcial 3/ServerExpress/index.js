092d4c53f10685571c7225cf8120aea8dc73f8c4
SteinRZ <xddan15@gmail.com>
2023-11-14 19:12:36 -0600

    Peticion Node y Peticion Express

--- Parcial 3/ServerExpress/index.js
+++ Parcial 3/ServerExpress/index.js
@@ -1,22 +1,22 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 
-app.get('/clientes', (req,res)=>{
+app.get('/', (req, res)=>{
     console.log(res.query)
     res.json({mensaje:"Server Express respondiendo a get"});
-})
+});
 
-app.post('/clientes', (req,res)=>{
+app.post('/', (req,res)=>{
     res.json({mensaje:"Server Express respondiendo a post"});
-})
+});
 
-app.delete('/clientes', (req,res)=>{
+app.delete('/', (req,res)=>{
     res.json({mensaje:"Server Express respondiendo a delete"});
-})
+});
 
-app.listen(8082, ()=>{
+app.listen(8082, (req,res)=>{
     console.log("Servidor Express en puerto 8082");
-})
+});
