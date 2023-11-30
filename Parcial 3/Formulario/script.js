050285969d3f6a0f0b86c6e37bff617d9055c9fb
SteinRZ <xddan15@gmail.com>
2023-11-29 18:57:20 -0600

    Generacion de archivos PDF despues de una consulta

--- Parcial 3/Formulario/script.js
+++ Parcial 3/Formulario/script.js
@@ -1,4 +1,4 @@
-window.onload=function(){
+window.onload = function () {
     //? Traer los ID y titulos de las peliculas
     new gridjs.Grid({
         search: true,
@@ -8,38 +8,38 @@ window.onload=function(){
         height: '200px',
         width: '1200px',
         pagination: {
-            limit: 20
+            limit: 50
         },
         columns: ['ID', 'Titulo'],
         server: {
             url: 'http://localhost:8082',
-            then: data => data.map(pelicula => 
+            then: data => data.map(pelicula =>
                 [pelicula.ID_PELICULA, pelicula.TITULO]
             )
-            } 
+        }
     }).render(document.getElementById("tabla"));
 
     //? Boton para consultar un elemento especifico
     document.getElementById("btnConsultar").addEventListener("click", async () => {
-            let id = document.getElementById("inputID").value;
-            let response = await fetch(`http://localhost:8082/pelicula?ID=${id}`, {method:"GET"});
-            let data = await response.json();
-            console.log(data)
-            if(data.status==0) {
-                alert(data.mensaje);
-                console.log(data.status);
-            }
-            else {
-                alert(data.mensaje);
-                console.log(data.datos);
-                console.log(data.status);
-                document.getElementById("titulo").value=data.datos.TITULO;
-                document.getElementById("fecha").value=data.datos.FECHA_LANZAMIENTO;
-                document.getElementById("cast").value=data.datos.CAST;
-                document.getElementById("director").value=data.datos.DIRECTOR;
-                document.getElementById("productora").value=data.datos.PRODUCTORA;
-            }
-        });
+        let id = document.getElementById("inputID").value;
+        let response = await fetch(`http://localhost:8082/pelicula?ID=${id}`, { method: "GET" });
+        let data = await response.json();
+        console.log(data)
+        if (data.status == 0) {
+            alert(data.mensaje);
+            console.log(data.status);
+        }
+        else {
+            alert(data.mensaje);
+            console.log(data.datos);
+            console.log(data.status);
+            document.getElementById("titulo").value = data.datos.TITULO;
+            document.getElementById("fecha").value = data.datos.FECHA_LANZAMIENTO;
+            document.getElementById("cast").value = data.datos.CAST;
+            document.getElementById("director").value = data.datos.DIRECTOR;
+            document.getElementById("productora").value = data.datos.PRODUCTORA;
+        }
+    });
 
     //? Boton para agregar un nuevo elemento
     document.getElementById("btnAgregar").addEventListener("click", async () => {
@@ -48,15 +48,15 @@ window.onload=function(){
         let vCast = document.getElementById("cast").value;
         let vDirector = document.getElementById("director").value;
         let vProductora = document.getElementById("productora").value;
-        await fetch(`http://localhost:8082/pelicula?TITULO=${vTitulo}&FECHA_LANZAMIENTO=${vFecha}&CAST=${vCast}&DIRECTOR=${vDirector}&PRODUCTORA=${vProductora}`, {method:"POST"})
+        await fetch(`http://localhost:8082/pelicula?TITULO=${vTitulo}&FECHA_LANZAMIENTO=${vFecha}&CAST=${vCast}&DIRECTOR=${vDirector}&PRODUCTORA=${vProductora}`, { method: "POST" })
             .then(response => response.json())
-                .then(data => {
-                    if (data.status === 1) {
-                        alert(data.mensaje);
-                    } else {
-                        alert(data.mensaje);
-                    }
-                })
+            .then(data => {
+                if (data.status === 1) {
+                    alert(data.mensaje);
+                } else {
+                    alert(data.mensaje);
+                }
+            })
     });
 
     //? Boton para modificar un elemento
@@ -67,28 +67,45 @@ window.onload=function(){
         let vCast = document.getElementById("cast").value;
         let vDirector = document.getElementById("director").value;
         let vProductora = document.getElementById("productora").value;
-        await fetch(`http://localhost:8082/pelicula?ID=${vID}&TITULO=${vTitulo}&FECHA_LANZAMIENTO=${vFecha}&CAST=${vCast}&DIRECTOR=${vDirector}&PRODUCTORA=${vProductora}`, {method:"PUT"})
+        await fetch(`http://localhost:8082/pelicula?ID=${vID}&TITULO=${vTitulo}&FECHA_LANZAMIENTO=${vFecha}&CAST=${vCast}&DIRECTOR=${vDirector}&PRODUCTORA=${vProductora}`, { method: "PUT" })
             .then(response => response.json())
-                .then(data => {
-                    if (data.status === 1) {
-                        alert(data.mensaje);
-                    } else {
-                        alert(data.mensaje);
-                    }
-                })
+            .then(data => {
+                if (data.status === 1) {
+                    alert(data.mensaje);
+                } else {
+                    alert(data.mensaje);
+                }
+            })
     });
 
     //? Boton para eliminar un elemento especifico
     document.getElementById("btnEliminar").addEventListener("click", async () => {
         let id = document.getElementById("inputID").value;
-        await fetch(`http://localhost:8082/pelicula?ID=${id}`, {method:"DELETE"})
+        await fetch(`http://localhost:8082/pelicula?ID=${id}`, { method: "DELETE" })
             .then(response => response.json())
-                .then(data => {
-                    if (data.status === 1) {
-                        alert(data.mensaje);
-                    } else {
-                        alert(data.mensaje);
-                    }
-                })
+            .then(data => {
+                if (data.status === 1) {
+                    alert(data.mensaje);
+                } else {
+                    alert(data.mensaje);
+                }
+            })
     });
-}
+
+    //? Boton para generar un PDF de la consulta
+    document.getElementById("btnPDF").addEventListener("click", async () => {
+        let vID = document.getElementById("inputID").value;
+        let vTitulo = document.getElementById("titulo").value;
+        let vFecha = document.getElementById("fecha").value;
+        let vCast = document.getElementById("cast").value;
+        let vDirector = document.getElementById("director").value;
+        let vProductora = document.getElementById("productora").value;
+        let response = await fetch(`http://localhost:8082/pelicula/formato?id=` + vID + `&titulo=` + vTitulo + `&fecha=` + vFecha + `&cast=` + vCast + `&director=` + vDirector + `&productora=` + vProductora, { method: "GET" });
+        let blob = await response.blob();
+        let url = window.URL.createObjectURL(blob);
+        let a = document.createElement('a');
+        a.href = url;
+        a.download = 'a4.pdf';
+        a.click();
+    });
+};
--- Parcial 3/ServerExpress/index.js
+++ Parcial 3/ServerExpress/index.js
@@ -1,8 +1,11 @@
-//? Modulos para la creacion del servidor
+//? Modulos para el servidor
 const express = require('express');
 const cors = require('cors');
 const mysql2 = require('mysql2');
 const app = express();
+const { jsPDF } = require("jspdf");
+const fs = require('fs');
+const path = require('path');
 app.use(cors()); //*Para evitar error de cors
 
 //? Conexion a la base de datos
@@ -14,10 +17,10 @@ const connection = mysql2.createConnection({
 });
 
 //? Consulta de todos las peliculas de la base de datos
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     console.log(req.query.ID_PELICULA);
     let consulta = ''
-    if(typeof(req.query.ID_PELICULA)=='undefined'){
+    if (typeof (req.query.ID_PELICULA) == 'undefined') {
         consulta = `select * from PELICULA`
     } else {
         consulta = `select * from PELICULA where ID_PELICULA=${req.query.ID_PELICULA}`
@@ -26,22 +29,22 @@ app.get('/', (req, res)=>{
 
     connection.query(
         consulta,
-        function(err, results, fields) {
-            if(results.length==0){
-                res.json({ mensaje:"Esta pelicula no esta registrada" });
+        function (err, results, fields) {
+            if (results.length == 0) {
+                res.json({ mensaje: "Esta pelicula no esta registrada" });
             }
-            else{
+            else {
                 res.json(results);
             }
         }
     );
 });
 
-//? Consulta de una sola pelicula
-app.get('/pelicula',(req,res)=>{
+//? Consulta de una pelicula
+app.get('/pelicula', (req, res) => {
     console.log(req.query.ID);
-    let consulta=''
-    if(typeof(req.query.ID)=='undefined'){
+    let consulta = ''
+    if (typeof (req.query.ID) == 'undefined') {
         consulta = `SELECT * FROM PELICULA`;
     }
     else {
@@ -50,50 +53,54 @@ app.get('/pelicula',(req,res)=>{
     console.log(consulta)
     connection.query(
         consulta,
-        function(err, results, fields) {
-            if(results.length==0){
-                res.json({ status:0,
-                    mensaje:"Este ID no esta registrado, intente con otro ID por favor",
-                    datos: {} });
-            } 
+        function (err, results, fields) {
+            if (results.length == 0) {
+                res.json({
+                    status: 0,
+                    mensaje: "Este ID no esta registrado, intente con otro ID por favor",
+                    datos: {}
+                });
+            }
             else {
-                res.json({ status:1,
-                        mensaje :"Pelicula encontrada exitosamente",
-                        datos: (results.length==1) ? results[0] : results });
+                res.json({
+                    status: 1,
+                    mensaje: "Pelicula encontrada exitosamente",
+                    datos: (results.length == 1) ? results[0] : results
+                });
             }
         }
     )
 });
 
 //? Agregacion de una pelicula
-app.post('/pelicula', (req,res)=>{
+app.post('/pelicula', (req, res) => {
     console.log(req.query);
     let sentenciaSQL = '';
-    if (typeof(req.query.TITULO) == 'undefined' || typeof(req.query.FECHA_LANZAMIENTO) == 'undefined' || typeof(req.query.CAST) == 'undefined' || typeof(req.query.DIRECTOR) == 'undefined' || typeof(req.query.PRODUCTORA) == 'undefined') {
-        res.json({ 
+    if (typeof (req.query.TITULO) == 'undefined' || typeof (req.query.FECHA_LANZAMIENTO) == 'undefined' || typeof (req.query.CAST) == 'undefined' || typeof (req.query.DIRECTOR) == 'undefined' || typeof (req.query.PRODUCTORA) == 'undefined') {
+        res.json({
             status: 0,
             mensaje: "Completa todos los campos por favor",
-            datos: {} 
+            datos: {}
         });
-    } 
+    }
     else {
         sentenciaSQL = `INSERT INTO PELICULA (TITULO, FECHA_LANZAMIENTO, CAST, DIRECTOR, PRODUCTORA) VALUES ('${req.query.TITULO}', '${req.query.FECHA_LANZAMIENTO}', '${req.query.CAST}', '${req.query.DIRECTOR}', '${req.query.PRODUCTORA}')`;
         console.log(sentenciaSQL);
         connection.query(
             sentenciaSQL,
-            function(err, results, fields) {
+            function (err, results, fields) {
                 console.log(results);
                 if (results && results.affectedRows == 1) {
-                    res.json({ 
+                    res.json({
                         status: 1,
                         mensaje: "Pelicula agregada exitosamente",
-                        datos: {} 
+                        datos: {}
                     });
                 } else {
-                    res.json({ 
+                    res.json({
                         status: 0,
                         mensaje: "Hubo un error al agregar la pelicula, por favor intenta de nuevo",
-                        datos: {} 
+                        datos: {}
                     });
                 }
             }
@@ -101,35 +108,35 @@ app.post('/pelicula', (req,res)=>{
     }
 });
 
-//? Modificacion de una sola pelicula
-app.put('/pelicula', (req,res)=>{
+//? Modificacion de la pelicula consultada
+app.put('/pelicula', (req, res) => {
     console.log(req.query);
     let sentenciaSQL = '';
-    if (typeof(req.query.ID) == 'undefined' || typeof(req.query.TITULO) == 'undefined' || typeof(req.query.FECHA_LANZAMIENTO) == 'undefined' || typeof(req.query.CAST) == 'undefined' || typeof(req.query.DIRECTOR) == 'undefined' || typeof(req.query.PRODUCTORA) == 'undefined') {
-        res.json({ 
+    if (typeof (req.query.ID) == 'undefined' || typeof (req.query.TITULO) == 'undefined' || typeof (req.query.FECHA_LANZAMIENTO) == 'undefined' || typeof (req.query.CAST) == 'undefined' || typeof (req.query.DIRECTOR) == 'undefined' || typeof (req.query.PRODUCTORA) == 'undefined') {
+        res.json({
             status: 0,
             mensaje: "Completa todos los campos por favor",
-            datos: {} 
+            datos: {}
         });
-    } 
+    }
     else {
         sentenciaSQL = `UPDATE PELICULA SET TITULO = '${req.query.TITULO}', FECHA_LANZAMIENTO = '${req.query.FECHA_LANZAMIENTO}', CAST = '${req.query.CAST}', DIRECTOR = '${req.query.DIRECTOR}', PRODUCTORA = '${req.query.PRODUCTORA}' WHERE ID_PELICULA = ${req.query.ID}`;
         console.log(sentenciaSQL);
         connection.query(
             sentenciaSQL,
-            function(err, results, fields) {
+            function (err, results, fields) {
                 console.log(results);
                 if (results && results.affectedRows == 1) {
-                    res.json({ 
+                    res.json({
                         status: 1,
                         mensaje: "Pelicula modificada exitosamente",
-                        datos: {} 
+                        datos: {}
                     });
                 } else {
-                    res.json({ 
+                    res.json({
                         status: 0,
                         mensaje: "Hubo un error al modificar la pelicula, por favor intenta de nuevo",
-                        datos: {} 
+                        datos: {}
                     });
                 }
             }
@@ -137,14 +144,16 @@ app.put('/pelicula', (req,res)=>{
     }
 });
 
-//? Eliminacion de una sola pelicula
-app.delete('/pelicula', (req,res)=>{
+//? Eliminacion de una pelicula
+app.delete('/pelicula', (req, res) => {
     console.log(req.query.ID);
-    let sentenciaSQL=''
-    if(typeof(req.query.ID)=='undefined'){
-        res.json({ status:0,
-            mensaje:"Ingresa el ID de la pelicula que deseas eliminar por favor",
-            datos: {} });
+    let sentenciaSQL = ''
+    if (typeof (req.query.ID) == 'undefined') {
+        res.json({
+            status: 0,
+            mensaje: "Ingresa el ID de la pelicula que deseas eliminar por favor",
+            datos: {}
+        });
     }
     else {
         sentenciaSQL = `DELETE FROM PELICULA WHERE ID_PELICULA = ${req.query.ID}`;
@@ -152,29 +161,55 @@ app.delete('/pelicula', (req,res)=>{
     console.log(sentenciaSQL);
     connection.query(
         sentenciaSQL,
-        function(err, results, fields) {
+        function (err, results, fields) {
             console.log(results);
-            if (results.affectedRows==1) {
-                res.json({ status:1,
-                    mensaje:"Pelicula eliminada exitosamente",
-                    datos: {} });
-            } 
+            if (results.affectedRows == 1) {
+                res.json({
+                    status: 1,
+                    mensaje: "Pelicula eliminada exitosamente",
+                    datos: {}
+                });
+            }
             else {
-                res.json({ status:0,
-                    mensaje:"Este ID no esta registrado, intente con otro ID por favor",
-                    datos: {} });
+                res.json({
+                    status: 0,
+                    mensaje: "Este ID no esta registrado, intente con otro ID por favor",
+                    datos: {}
+                });
             }
         }
     )
 });
 
-//? Mensaje de confirmacion de arranque de servidor
-app.listen(8082, (req,res)=>{
-    console.log("Servidor Express en puerto 8082");
+//? Crear PDF de la consulta
+app.get('/pelicula/formato', (req, res) => {
+    const doc = new jsPDF();
+    doc.setFontSize(12);
+    const titulo = req.query.titulo;
+    const fecha = req.query.fecha;
+    const cast = req.query.cast;
+    const director = req.query.director;
+    const productora = req.query.productora;
+    doc.text('Título de la película:', 10, 10);
+    doc.text(titulo, 10, 20);
+    doc.text('Fecha de lanzamiento:', 10, 40);
+    doc.text(fecha, 10, 50);
+    doc.text('Cast:', 10, 70);
+    doc.text(cast, 10, 80);
+    doc.text('Director:', 10, 100);
+    doc.text(director, 10, 110);
+    doc.text('Productora:', 10, 130);
+    doc.text(productora, 10, 140);
+    let archivoPDF = path.join('C:\\wamp64\\www\\RepositoriosGithub\\Parcial 3\\Formulario\\documents', 'consulta.pdf');
+    doc.save(archivoPDF, function (err) {
+        if (err) {
+            return res.sendStatus(500);
+        }
+        res.download(archivoPDF);
+    });
 });
 
-
-///! CODIGO SIN UTILIZAR ///
-/*
-
-*/
+//? Mensaje de confirmacion de arranque de servidor
+app.listen(8082, (req, res) => {
+    console.log("Servidor Express en puerto 8082");
+});
